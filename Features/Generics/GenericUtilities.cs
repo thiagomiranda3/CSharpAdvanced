2b93ca888e50786061cea46fc5e9ef4088b765a8
Thiago Miranda <marine.rlivielor@gmail.com>
2017-12-05 23:25:08 -0200

    Generic com construtor default

--- Features/Generics/GenericUtilities.cs
+++ Features/Generics/GenericUtilities.cs
@@ -2,12 +2,24 @@ using System;
 
 namespace Features.Generics
 {
+    // Constraints possíveis com Generics
+    // where T : IComparable (Constraints de interface)
+    // where T : Product     (Constraint de classe)
+    // where T : struct      (Constraint de Value Types)
+    // where T : class       (Constraint de Reference Type)
+    // where T : new()       (Constraint de tipo de construtor)
     public class GenericUtilities
     {
         // É possível criar classes ou métodos genéricos
         public T Max<T>(T a, T b) where T : IComparable
         {
             return a.CompareTo(b) > 0 ? a : b;
+        }
+
+        // Para instanciar uma Generic, é necessário um constraint de construtor default
+        public void InstanciarGeneric<T>(T value) where T : new()
+        {
+            var obj = new T();
         }
     }
 }
